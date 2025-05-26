3eaba04dd9f83184547094cb8b5f96d02f30c10e
SamClosyss <sanmeet.gidda@closyss.com>
2025-05-26 10:12:38 +0530

    changes in code

--- frontend/movie/src/customHooks/useFetchMovieApi.js
+++ frontend/movie/src/customHooks/useFetchMovieApi.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { default as axios } from "axios";
 
+ let mainUrl = "https://api.themoviedb.org/3/movie/";
+
 export default function useFetchMovieApi(props) {
   const [data, setData] = useState([]);
-  let mainUrl = "https://api.themoviedb.org/3/movie/";
 
   useEffect(() => {
     axios.get(mainUrl + props).then((resp) => setData(resp.data.results));
